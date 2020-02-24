5178965c97e8c63ab8d7ebb3a4c86718d0993239
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2020-02-24 11:24:48 +0000

    Add configuration helper code

---
+++ src/test/java/pt/tecnico/sd/ConfigHelperTest.java
@@ -0,0 +1,39 @@
+package pt.tecnico.sd;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+
+/**
+ * Unit tests for ConfigHelper.
+ */
+public class ConfigHelperTest {
+
+    @Test
+    public void testGetExistingProperty() {
+        ConfigHelper config = new ConfigHelper();
+        String value = config.getValue("min");
+        assertNotNull(value);
+        /* expected value, actual value */
+        assertEquals("7", value);
+    }
+
+    @Test
+    public void testGetFilteredProperty() {
+        ConfigHelper config = new ConfigHelper();
+        String value = config.getValue("max");
+        assertNotNull(value);
+        /* expected value, actual value */
+        assertEquals("22", value);
+    }
+
+    @Test
+    public void testGetMissingProperty() {
+        ConfigHelper config = new ConfigHelper();
+        String value = config.getValue("not_there");
+        assertNull(value);
+    }
+
+}
